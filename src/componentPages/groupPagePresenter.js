7a97ffa19df6255fdef4999dbbec08a241eaae1b
vilmelhina <vilhelmina2.0@gmail.com>
2023-03-14 18:41:20 +0100

    Static version of group page, to be filled with actual data

--- src/componentPages/groupPagePresenter.js
+++ src/componentPages/groupPagePresenter.js
@@ -2,6 +2,35 @@ import React from "react";
 import GroupPageView from "./groupPageView";
 
 function GroupPage() {
-    return <GroupPageView/>
+
+    // TODO: actual values instead of dummy values
+
+    const groupName = "groupname";
+    const userCells = [...Array(25).keys()].map(element => {return {text: "cell" + element, done: false}});
+    const friendsProgress = [
+        {name: "name1", progress: [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]},
+        {name: "name2", progress: [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]},
+        {name: "name3", progress: [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]}
+    ]
+    const scoreBoard = [
+        {name: "name1", score: 444},
+        {name: "name2", score: 333},
+        {name: "name3", score: 888}
+    ]
+    const role = "admin";
+
+    function cellToggled(index) {
+        // TODO: update state
+        console.log("clicked " + index)
+    }
+
+    return <GroupPageView
+                groupName = {groupName}
+                userCells = {userCells}
+                friendsProgress = {friendsProgress}
+                scoreBoard = {scoreBoard}
+                role = {role}
+                cellToggled = {cellToggled}
+            />
 }
 export default GroupPage;
--- src/componentPages/groupPageView.js
+++ src/componentPages/groupPageView.js
@@ -1,7 +1,69 @@
 function GroupPageView(props){
+
+    function doneClass(done) {
+        if(done) return "cell marked";
+        return "cell unmarked";
+    }
+
+    function displayCellCB(cell, index) {
+        return (
+            <div className={doneClass(cell.done)} key={index}
+                 onClick={() => {props.cellToggled(index)}}>
+                {cell.text}
+            </div>)
+    }
+
+    function displayFriendsProgressCB(friend, index) {
+        function displayFriendCellCB(done, index) {
+            return <div className={doneClass(done)} key={"cell" + index}/>
+        }
+        return (
+            <div className="friend" key={index}>
+                <div className="board friend-board">
+                    {friend.progress.map(displayFriendCellCB)}
+                </div>
+                {friend.name}
+            </div>
+        )
+    }
+
+    function displayScoreCB(item, index) {
+        return <li key={index}>{item.name} - {item.score}</li>
+    }
+
     return (
-        <div>
-            group
+        <div id="group-container">
+            <div className="board-container">
+                <h2>{props.groupName}</h2>
+                <div className="board user-board">
+                    {props.userCells.map(displayCellCB)}
+                </div>
+            </div>
+            <div className="sidebar">
+                <div className="section">
+                    <h3>Friends progress</h3>
+                    <div className="friends-progress-container">
+                        {props.friendsProgress.map(displayFriendsProgressCB)}
+                    </div>
+                </div>
+                <div className="section">
+                    <h3>Group scoreboard</h3>
+                    <ol className="score-board">
+                        {props.scoreBoard.map(displayScoreCB)}
+                    </ol>
+                </div>
+                {props.role === "admin" ? // TODO: actions for buttons
+                    <div className="section">
+                        <h3>Admin</h3>
+                        <div className="button-container">
+                            <button>Handle members</button>
+                            <button>Handle bingo cells</button>
+                            <button>Generate new boards</button>
+                            <button>Settings</button>
+                        </div>
+                    </div>
+                 : ""}
+            </div>
         </div>
     );
 }
